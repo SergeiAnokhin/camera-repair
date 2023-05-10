254ee96d840508d90c439f8567ec8f425d544876
Sergei Anokhin <sa.anokhin@yandex.ru>
2023-05-10 10:28:39 -0700

    add english translation

--- README.md
+++ README.md
@@ -1,22 +1,32 @@
-# Проект: Ремонт плёночных фотоаппаратов
 
-**Описание проекта:**
+# Film camera repair
 
-Индивидуальная практическая работа на онлайн-курсе "Веб-разработчик плюс" от Яндекс.Практикума. Работа представляет собой одностраничный лендинг о ремонте плёночных фотоаппаратов. 
+## [Project Link](https://sergeianokhin.github.io/camera-repair/index.html)
 
-**В проекте использованы:**
+## Tech Stack
 
-- Семантическая разметка;
-- Методология БЭМ;
-- Адаптивная верстка под мобильные устройства;
-- Модели построения сеток Flex;
-- Препроцессор SCSS;
-- JavaScript.
+HTML, CSS, SCSS, BEM, JavaScript
 
----
+## Main goal
 
-**Студент:** Сергей Анохин
+Practicing in responsive layout using preprocessor SCSS
 
-[Ссылка на GitHub Pages](https://sergeianokhin.github.io/camera-repair/index.html)
+## Description
 
-![Логотип Яндекса](./images/yandex-logo.svg)
+This project is individual practical work demonstrates **responsive layout for different devices.** 
+
+The project are used:
+ - semantic markup
+ - BEM methodology
+ - CSS flexbox layout
+ - SCSS preprocessor
+ - JavaScript for demonstrate sending email option with simple email  input validation
+
+## Setup
+
+There are two options:
+
+- Run index.html using your browser
+- Download project or use git clone. Than run the project from you IDE. You can use LiveServer extension for VS Code for example
+
+![Preview](./readme_static/review.png)
--- scripts/index.js
+++ scripts/index.js
@@ -6,32 +6,45 @@ const footerForm = document.querySelector('#footer-form');
 const footerInput = footerForm.querySelector('input');
 const footerBtn = footerForm.querySelector('button');
 
+const timeOut = (input, button) => setTimeout(() => {
+    if (window.innerWidth > 500) {
+        button.innerHTML = 'Thank you for your trust!';
+    };
+
+    if (window.innerWidth < 500) {
+        button.innerHTML = '👌';
+    };
+
+    input.value = '';
+}, 900);
+
+const timeOutAfterSendingEmail = (button) => setTimeout(() => {
+    if (window.innerWidth > 500) {
+        button.innerHTML = 'Repair your camera ✌️';
+    };
+
+    if (window.innerWidth < 500) {
+        button.innerHTML = '✌️';
+    };
+}, 3000);
+
+
 repairRequestForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (repairRequestInput.value) {
-
-        console.log(repairRequestInput.value)
-
-        if (window.innerWidth > 500) {
-            repairRequestBtn.innerHTML = 'Круто, спасибо за доверие!'
-        }
-
+        console.log(repairRequestInput.value);
+        timeOut(repairRequestInput, repairRequestBtn);
+        timeOutAfterSendingEmail(repairRequestBtn);
     }
-
-})
+});
 
 footerForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (footerInput.value) {
-
-        console.log(footerInput.value)
-
-        if (window.innerWidth > 500) {
-            footerBtn.innerHTML = 'Круто, спасибо за доверие!'
-        }
-
-    }
-
-})
+        console.log(footerInput.value);
+        timeOut(footerInput, footerBtn);
+        timeOutAfterSendingEmail(footerBtn);
+    };
+});
